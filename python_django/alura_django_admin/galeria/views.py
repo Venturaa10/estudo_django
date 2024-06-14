ca5ecde51dc63cf762cc3e7fecc218b04153262f
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-06-13 21:46:45 -0300

    Tornei o arquivo imagem mais dinamica, através do id do objeto no banco de dados

--- python_django/alura_django_admin/galeria/views.py
+++ python_django/alura_django_admin/galeria/views.py
@@ -1,9 +1,11 @@
 from galeria.models import Fotografia
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     fotografias = Fotografia.objects.all()
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    '''get_object_or_404 -> Retorna o objeto que é o id do objeto no banco de dados, caso não encontre, retorna um erro 404'''
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
