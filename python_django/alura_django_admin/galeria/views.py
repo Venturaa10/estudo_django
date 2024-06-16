4dff5aaac3b78890c5b788681f05ab1146a131be
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-06-16 18:55:16 -0300

    Adicionei a funcionalidade de busca

--- python_django/alura_django_admin/galeria/views.py
+++ python_django/alura_django_admin/galeria/views.py
@@ -11,3 +11,15 @@ def imagem(request, foto_id):
     '''get_object_or_404 -> Retorna o objeto que é o id do objeto no banco de dados, caso não encontre, retorna um erro 404'''
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+    
+    if 'buscar' in request.GET:
+        '''O "buscar" faz referencia ao "name" do input de busca'''
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            '''Faz referencia ao nome que estou buscando e o nome do objeto em que está sendo buscado'''
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+        
+    return render(request, 'galeria/buscar.html', {"cards": fotografias})
