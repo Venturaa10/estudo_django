40265d2fe897fa8b6e15d05271fa32ddf9cffc94
Joao Victor Ventura <joao.victor.ventura25@gmail.com>
2024-06-18 21:45:14 -0300

    Estilizando as tags 'label' do HTML diretamente do atributo do objeto

--- alura_django_admin/usuarios/forms.py
+++ alura_django_admin/usuarios/forms.py
@@ -5,12 +5,25 @@ class LoginForms(forms.Form):
     nome_login=forms.CharField(
         label='Nome de Login',
         required=True, # Torna o preenchimento desse campo obrigatorio
-        max_length= 100
+        max_length= 100,
+        widget=forms.TextInput(
+            # Estilizando os labels do HTML através do proprio atributo do objeto
+            attrs={
+                'class': 'form-control', 
+                'placeholder': 'Ex.: João Victor'
+            }
+        )
     )
     
     senha=forms.CharField(
         label='Senha',
         required=True, # Torna o preenchimento desse campo obrigatorio
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            # 
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite a sua senha'
+            }
+        )
     )
